b9abe196533c9377a6c64aa5398cc24402644dad
Mai El-Awini <melawini@ad-dispatch.com>
2013-10-01 20:51:24 -0300

    model importer

---
+++ AssetPostProcessors/ModelImporter/ClipXMLParser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Collections;
+
+
+/// <summary>
+/// Clip XML parser.
+/// Parses an XML file for clip attributes that get set when splitting a timeline. 
+/// </summary>
+public class ClipXMLParser {
+
+	private XmlDocument docXml;
+	private int _clipCount;
+
+
+	public ClipXMLParser(string filePath){
+		docXml = new XmlDocument();
+		docXml.Load(filePath);
+
+		if(docXml == null){
+			Debug.LogError("Your XML file needs to share the same path and file name as the model it references.");
+		}
+	}
+
+	public XmlNodeList GetClips(){
+		XmlNode rootNode = docXml.SelectSingleNode("timeline");
+		return rootNode.SelectNodes("clip");
+	}
+
+	public string GetClipAttribute(string attribute, XmlNode clipNode){
+		return clipNode.SelectSingleNode(attribute).InnerText;
+	}
+
+}
---
+++ AssetPostProcessors/ModelImporter/SplitTimelinePostprocessor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Xml;
+
+
+/// <summary>
+/// SplitTimelinePostprocessor
+/// Mainly used for splitting a model's timeline at import. 
+/// Reads from an XML file that follows the format in Template.xml
+/// Some other settings are set on the model. These just happen to be the settings I always use on importing.
+/// Adding/removing some setting is straight forward.
+/// </summary>
+public class SplitTimelinePostprocessor : AssetPostprocessor {
+
+	void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = assetImporter as ModelImporter;
+
+		//Some personal settings I always make
+		modelImporter.globalScale = 0.1f;
+		modelImporter.animationType = ModelImporterAnimationType.Legacy;
+		modelImporter.bakeIK = true;
+		modelImporter.importMaterials = true;
+
+
+		//Splitting the timeline based on the xml file provided by the artist
+
+		//Find the xml file and parse it
+		//We expect to find the xml file in the same folder as the model and to share the same name
+		string filePath = Path.GetDirectoryName(assetPath) + "/Timelines/" + Path.GetFileNameWithoutExtension(assetPath);
+		ClipXMLParser parser = new ClipXMLParser(filePath + ".xml");
+
+		//Get the list od clips we pulled from the xml data
+		XmlNodeList clipNodes = parser.GetClips();
+		ModelImporterClipAnimation[] clips = new ModelImporterClipAnimation[clipNodes.Count];
+
+		//Create a ModelImporterClipAnimation for each clip and populate the array
+		for(int i=0; i<clipNodes.Count; i++)
+		{		
+			ModelImporterClipAnimation mica = new ModelImporterClipAnimation();
+			mica.name 		= parser.GetClipAttribute("name", clipNodes[i]);
+			mica.firstFrame = Int32.Parse( parser.GetClipAttribute("firstFrame", clipNodes[i]) );
+			mica.lastFrame 	= Int32.Parse( parser.GetClipAttribute("lastFrame", clipNodes[i]) );
+			mica.loop 		= bool.Parse( parser.GetClipAttribute("loop", clipNodes[i]) );
+			mica.wrapMode 	= mica.loop ? WrapMode.Loop : WrapMode.Default;
+
+			clips[i] = mica;
+		}
+	
+		modelImporter.clipAnimations = clips;
+
+	}
+
+
+}
