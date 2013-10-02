40105defe5d6800e3b74401353faf75f7be93fa5
Mai El-Awini <melawini@ad-dispatch.com>
2013-10-01 21:21:32 -0300

    GUI importer

---
+++ AssetPostProcessors/GUIImporter/GUITexturePostprocessor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+
+
+/// <summary>
+/// GUITexturePostprocessor
+/// Sets Texture Type = GUI and Format = Truecolor for all textures imported into the Atlases_Slices folder.
+/// </summary>
+public class GUITexturePostprocessor : AssetPostprocessor {
+
+
+	void OnPreprocessTexture()
+	{
+
+		TextureImporter textureImporter = assetImporter as TextureImporter;
+
+		DirectoryInfo path = new DirectoryInfo(assetPath);
+		Debug.Log(path.Parent.Name);
+		if(path.Parent.Name == "Atlases_Slices")
+		{
+			textureImporter.textureType = TextureImporterType.GUI;
+			textureImporter.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+		}
+
+	}
+
+}
